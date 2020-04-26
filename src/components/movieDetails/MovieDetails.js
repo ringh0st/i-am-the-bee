b47055cf22f85908cebcaf38c18fdebaff1c97d1
Rinat Nadav <5136880-ringhost@users.noreply.gitlab.com>
2020-04-26 18:32:32 +0300

    added api variable

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -19,15 +19,15 @@ class MovieDetails extends React.Component {
     async componentDidMount() {
         // const omdbRes = await omdb.get(`i=${movieById.data.imdb_id}`);
         // console.log(omdbRes);
-        const omdb_key = "bc901512&i";
+        const omdb_key = "bc901512";
         const tmdb_key = "b0a20e995baa08cdd818c57bcd38ffd1";
         this.setState({ isLoading: true });
         const { id } = this.props.match.params
-        const movieById = await tmdb.get(`movie/${id}?${tmdb_key}&language=en-US&include_video=true`);
+        const movieById = await tmdb.get(`movie/${id}?api_key=${tmdb_key}&language=en-US&include_video=true`);
         const imdbId = await movieById.data.imdb_id;
-        const trailer = await tmdb.get(`movie/${imdbId}/videos?${tmdb_key}&language=en-US`);
-        const omdbRes = await omdb.get(`?${omdb_key}=${imdbId}`);
-        const peoplesId = await tmdb.get(`movie/${id}/credits?${tmdb_key}&language=en-US`)
+        const trailer = await tmdb.get(`movie/${imdbId}/videos?api_key=${tmdb_key}&language=en-US`);
+        const omdbRes = await omdb.get(`?apikey=${omdb_key}&i=${imdbId}`);
+        const peoplesId = await tmdb.get(`movie/${id}/credits?api_key=${tmdb_key}&language=en-US`)
         // console.log(omdbRes);
         // console.log(movieById);
         // console.log(peoplesId);
@@ -78,9 +78,9 @@ class MovieDetails extends React.Component {
     render() {
         const spinner = () => {
             return (
-                <div class="ui segment">
-                    <div class="ui active dimmer">
-                        <div class="ui large text loader">Preparing Files</div>
+                <div className="ui segment">
+                    <div className="ui active dimmer">
+                        <div className="ui large text loader">Preparing Files</div>
                     </div>
                     <p></p>
                 </div>
